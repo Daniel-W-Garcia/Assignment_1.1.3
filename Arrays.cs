d78b8a14e60cffc62c0df52ed12f294520d45637
itodobien <daniel.w.garcia@outlook.com>
2025-04-09 12:31:55 -0700

    Added Clarity
    Included printout out of the arrays into the console to make it clearer to the user

--- Arrays.cs
+++ Arrays.cs
@@ -3,8 +3,11 @@
 public class Arrays
 {
     private string[] names;
-    private int[] numbers = {1, 3, 5, 7, 9};
-    private double[] doubles = {2.4, 3.5, 4.6, 5.7, 6.8};
+    private int[] integersArray = {1, 3, 5, 7, 9};
+    private int[] reversedIntArray;
+    private double[] doublesArray = {2.4, 3.5, 4.6, 5.7, 6.8};
+    private double[] reversedDoublesArray;
+    
     
     private string[,] twoDinmensionalArray = new string[5, 5];
     private string[,,] threeDimensionalArray = new string[5, 5, 5];
@@ -36,10 +39,9 @@ public class Arrays
             answer = Console.ReadLine();
             if (answer.ToLower() == "y")
             {
-                ReverseArray();
+                ReverseArrayOfNames();
                 Console.WriteLine("\nPress any key to return to the array menu...");
                 Console.ReadKey();
-                    
             }
             else
             {
@@ -65,11 +67,20 @@ public class Arrays
     public void SumOfAllIntegers()
     {
         int sum = 0;
-        foreach (int number in numbers)
+        foreach (int number in integersArray)
         {
             sum += number;
         }
-        Console.WriteLine(sum);
+        
+        reversedIntArray = ReverseArrayOfIntegers(integersArray);
+
+        Console.WriteLine($"""
+                           The array containing {string.Join(", ", integersArray)} 
+                           has a sum of: {sum}
+                           """);
+        Console.WriteLine($"""
+                          and a reversed order of: {string.Join(", ", reversedIntArray)}
+                          """);
         
         Console.WriteLine("\nPress any key to return to the array menu...");
         Console.ReadKey();
@@ -78,24 +89,55 @@ public class Arrays
     public void SumOfAllDoubles()
     {
         double sum = 0;
-        for (int i =0; i < doubles.Length; i++)
+        for (int i =0; i < doublesArray.Length; i++)
         {
-            sum += doubles[i];
+            sum += doublesArray[i];
         }
-        Console.WriteLine(sum);
+        
+        reversedDoublesArray = ReverseArrayOfDoubles(doublesArray);
+        
+        Console.WriteLine($"""
+                           The array containing {string.Join(", ", doublesArray)} 
+                           has a sum of: {sum}
+                           """);
+        double[] reversedDoubles = ReverseArrayOfDoubles(doublesArray);
+        Console.WriteLine($"""
+                           and a reversed order of: {string.Join(", ", reversedDoubles)}
+                           """);
         
         Console.WriteLine("\nPress any key to return to the array menu...");
         Console.ReadKey();
     }
 
-    public void ReverseArray()
+    public void ReverseArrayOfNames()
     {
         string[] reversedArray = new string[names.Length];
+        
         for (int i = 0; i < names.Length; i++)
         {
             reversedArray[i] = names[names.Length - i - 1];
         }
         PrintNames(reversedArray);
+    }
+
+    public int[] ReverseArrayOfIntegers(int[] integersArray)
+    {
+        int[] reversedIntArray = new int[integersArray.Length];   
+        for (int i = 0; i < integersArray.Length; i++)
+        {
+            reversedIntArray[i] = integersArray[integersArray.Length - i - 1];
+        }
+        return reversedIntArray;
+    }
+
+    public double[] ReverseArrayOfDoubles(double[] doublesArray)
+    {
+        double[] reversedDoublesArray = new double[doublesArray.Length];
+        for (int i = 0; i < doublesArray.Length; i++)
+        {
+            reversedDoublesArray[i] = doublesArray[doublesArray.Length - i - 1];
+        }
+        return reversedDoublesArray;
     }
 
     public void ClearScreenAndReturnToMenu(UserInterface userInterface)
